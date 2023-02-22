e0121f5cdbfc1e335d5e31daffc485c391615736
maskharor.amnan05@gmail.com <maskharor.amnan05@gmail.com>
2023-02-22 23:11:23 +0700

    add carousel

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
 import Intro from "../components/UI/Intro";
+import Carousel from "../components/UI/Carousel";
 
 export default function Home() {
   return (
     <Fragment>
+      <Carousel />
       <Intro />
     </Fragment>
   );
