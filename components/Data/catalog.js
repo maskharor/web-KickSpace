7218deed73af6e93acb57871112d881817652406
maskharor.amnan05@gmail.com <maskharor.amnan05@gmail.com>
2023-02-23 00:25:19 +0700

    add catalog adidas shoes

--- components/Data/catalog.js
+++ components/Data/catalog.js
@@ -42,7 +42,7 @@ const catalog = [
         liveUrl: "#",
     },
     {
-        id: "6",
+        id: "06",
         title: "Lebron XX",
         img: "/images/catalog-img6.jpg",
         category: "Nike",
@@ -50,7 +50,7 @@ const catalog = [
         liveUrl: "#",
     },
     {
-        id: "7",
+        id: "07",
         title: "Jordan Luka 1",
         img: "/images/catalog-img7.jpg",
         category: "Nike",
@@ -58,13 +58,86 @@ const catalog = [
         liveUrl: "#",
     },
     {
-        id: "8",
+        id: "08",
         title: "Air Jordan 3 Retro",
         img: "/images/catalog-img8.jpg",
         category: "Nike",
         keyword: ["Nike", "Casual", "Style"],
         liveUrl: "#",
     },
+
+  // ========================== Nike Shoes ===================    
+
+    {
+        id: "09",
+        title: "Dame Extply 2.0 (Certified)",
+        img: "/images/catalog-img9.jpg",
+        category: "Adidas",
+        keyword: ["Adidas", "Sport", "Basket"],
+        liveUrl: "#",
+      },
+    
+      {
+        id: "10",
+        title: "Adidas NMD",
+        img: "/images/catalog-img10.jpg",
+        category: "Adidas",
+        keyword: ["Adidas", "Sport", "Running"],
+        liveUrl: "#",
+      },
+    
+      {
+        id: "11",
+        title: "Adidas Superstar -Black",
+        img: "/images/catalog-img11.jpg",
+        category: "Adidas",
+        keyword: ["Adidas", "Casual", "Style"],
+        liveUrl: "#",
+      },
+    
+      {
+        id: "12",
+        title: "Adidas Court Silk",
+        img: "/images/catalog-img12.jpg",
+        category: "Adidas",
+        keyword: ["Adidas", "Sport", "Running"],
+        liveUrl: "#",
+      },
+    
+      {
+        id: "13",
+        title: "Adidas Superstar -White",
+        img: "/images/catalog-img13.jpg",
+        category: "Adidas",
+        keyword: ["Adidas", "Sport", "Running"],
+        liveUrl: "#",
+      },
+    
+      {
+        id: "14",
+        title: "Adidas Harden VOL. 6",
+        img: "/images/catalog-img14.jpg",
+        category: "Adidas",
+        keyword: ["Adidas", "Sport", "Basket"],
+        liveUrl: "#",
+      },
+    
+      {
+        id: "15",
+        title: "Dame 8",
+        img: "/images/catalog-img15.jpg",
+        category: "Adidas",
+        keyword: ["Adidas", "Sport", "Basket"],
+        liveUrl: "#",
+      },
+      {
+        id: "16",
+        title: "Yeezy Boost 350",
+        img: "/images/catalog-img16.jpg",
+        category: "Adidas",
+        keyword: ["Adidas", "Casual", "Style"],
+        liveUrl: "#",
+      },
 ];
 
 export default catalog;
