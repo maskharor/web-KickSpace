496506ae0769d08e3a92f206b29e2ff80e8414f5
maskharor.amnan05@gmail.com <maskharor.amnan05@gmail.com>
2023-03-03 10:45:12 +0700

    fix error vercel

--- pages/[slug]/index.js
+++ pages/[slug]/index.js
@@ -1,3 +1,4 @@
+"use client"
 import data from "../../components/data/catalog";
 import React, { useEffect, useState, useRef } from "react";
 import Image from "next/image";
@@ -17,24 +18,29 @@ function Order() {
   const router = useRouter();
   const { slug } = router.query;
 
+  const [qty, setQty] = useState(1);
+
   const shoes = getShoes(slug);
 
   if (Object.keys(shoes).length === 1) {
     return <div>Item not found</div>;
   }
 
-  const [qty, setQty] = useState(1);    
-  const total = qty * shoes.price
+
+  const total = qty * shoes.price;
+
   return (
     <>
       <div className="container lg:max-w-screen-lg mx-auto py-24 px-6">
         <div className="flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0 md:space-x-12">
           {/* Product's image */}
           <div className="relative">
-            <img
+            <Image
               src={shoes.img}
               alt={shoes.title}
               className="w-96 rounded-md"
+              width={200}
+              height={200}
             />
           </div>
 
@@ -43,20 +49,22 @@ function Order() {
             <h2 className="text-3x2 font-semibold">{shoes.title}</h2>
             <p>
               <span className="text-white-500">Stok:</span>{" "}
-              <span className="font-semibold text-orange-500">{shoes.stok}</span>
+              <span className="font-semibold text-orange-500">
+                {shoes.stok}
+              </span>
             </p>
 
             {/* Price */}
             <div className="mt-8 border-t pt-4">
               <p className="text-white-500">Harga:</p>
-                <h3 className="text-2x1 font-semibold">{shoes.price}</h3>
+              <h3 className="text-2x1 font-semibold">{shoes.price}</h3>
             </div>
 
             <div className="mt-4 border-t pt-4">
               <p className="text-white-500">Jumlah:</p>
               <div className="mt-1 flex items-center space-x-3">
                 <button
-                  onClick={() => setQty(prev => prev - 1)}
+                  onClick={() => setQty((prev) => prev - 1)}
                   disabled={qty <= 1}
                   className="text-white disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:hover:text-current hover:bg-rose-100 hover:text-rose-500 rounded-md p-1 border-0"
                 >
@@ -64,8 +72,8 @@ function Order() {
                 </button>
                 <p className="font-semibold text-xl text-orange-500">{qty}</p>
                 <button
-                  onClick={() => setQty(prev => prev + 1)}
-                  disabled={qty >= (shoes.stok)}
+                  onClick={() => setQty((prev) => prev + 1)}
+                  disabled={qty >= shoes.stok}
                   className="text-white disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:hover:text-current hover:bg-green-100 hover:text-green-500 rounded-md p-1 border-0"
                 >
                   <Plus />
